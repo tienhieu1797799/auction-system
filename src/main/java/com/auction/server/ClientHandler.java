5621b99ebf7ce4864087767bfa2b990c96ca93c4
tienhieu1797799 <25023473@vnu.edu.vn>
2026-04-06 15:46:50 +0700

    Add ClientHandler class implementation

--- src/main/java/com/auction/server/ClientHandler.java
+++ src/main/java/com/auction/server/ClientHandler.java
@@ -1,66 +1,115 @@
+package com.auction.server;
+
+import com.auction.model.*;
 import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.net.Socket;
+import java.util.Map;
 
 public class ClientHandler implements Runnable {
-    private Socket clientSocket;
+    private Socket socket;
+    private AuctionServer server;
     private ObjectInputStream input;
     private ObjectOutputStream output;
-    private AuctionServer server;
+    private User currentUser;
 
     public ClientHandler(Socket socket, AuctionServer server) {
-        this.clientSocket = socket;
+        this.socket = socket;
         this.server = server;
     }
 
     @Override
     public void run() {
         try {
-            output = new ObjectOutputStream(clientSocket.getOutputStream());
-            input = new ObjectInputStream(clientSocket.getInputStream());
+            output = new ObjectOutputStream(socket.getOutputStream());
+            output.flush();
+            input = new ObjectInputStream(socket.getInputStream());
 
-            processClientMessages();
-        } catch (IOException e) {
+            Message message;
+            while ((message = (Message) input.readObject()) != null) {
+                handleMessage(message);
+            }
+        } catch (EOFException e) {
+            System.out.println("Client disconnected");
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         } finally {
-            closeConnections();
+            server.removeClient(this);
+            closeConnection();
         }
     }
 
-    private void processClientMessages() throws IOException {
-        Object message;
-        while ((message = input.readObject()) != null) {
-            processMessage(message);
+    private void handleMessage(Message message) {
+        switch (message.getType()) {
+            case LOGIN:
+                handleLogin((String[]) message.getData());
+                break;
+            case GET_AUCTIONS:
+                handleGetAuctions();
+                break;
+            case PLACE_BID:
+                handlePlaceBid((String[]) message.getData());
+                break;
+            default:
+                break;
         }
     }
 
-    private void processMessage(Object message) {
-        if (message instanceof LoginMessage) {
-            handleLogin((LoginMessage) message);
-        } else if (message instanceof AuctionRequest) {
-            handleAuctionRetrieval((AuctionRequest) message);
-        } else if (message instanceof BidPlacement) {
-            handleBidPlacement((BidPlacement) message);
+    private void handleLogin(String[] credentials) {
+        String username = credentials[0];
+        String password = credentials[1];
+        
+        User user = server.authenticateUser(username, password);
+        if (user != null) {
+            currentUser = user;
+            Message response = new Message(Message.MessageType.SUCCESS, 
+                "Login successful", user);
+            sendMessage(response);
+        } else {
+            Message response = new Message(Message.MessageType.ERROR, 
+                "Invalid credentials", null);
+            sendMessage(response);
         }
     }
 
-    private void handleLogin(LoginMessage login) {
-        // Handle client login logic here
+    private void handleGetAuctions() {
+        Map<String, Auction> auctions = server.getAuctions();
+        Message response = new Message(Message.MessageType.SUCCESS, 
+            "Auctions retrieved", auctions);
+        sendMessage(response);
     }
 
-    private void handleAuctionRetrieval(AuctionRequest auctionRequest) {
-        // Handle auction retrieval logic here
+    private void handlePlaceBid(String[] bidData) {
+        String auctionId = bidData[0];
+        double bidAmount = Double.parseDouble(bidData[1]);
+
+        boolean success = server.placeBid(auctionId, currentUser.getUserId(), 
+            currentUser.getUsername(), bidAmount);
+
+        Message response;
+        if (success) {
+            response = new Message(Message.MessageType.SUCCESS, 
+                "Bid placed successfully", null);
+        } else {
+            response = new Message(Message.MessageType.ERROR, 
+                "Bid failed. Amount too low or insufficient balance", null);
+        }
+        sendMessage(response);
     }
 
-    private void handleBidPlacement(BidPlacement bidPlacement) {
-        // Handle bid placement logic here
-    }
-
-    private void closeConnections() {
+    public void sendMessage(Message message) {
         try {
+            output.writeObject(message);
+            output.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeConnection() {
+        try {
+            if (socket != null) socket.close();
             if (input != null) input.close();
             if (output != null) output.close();
-            if (clientSocket != null) clientSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
